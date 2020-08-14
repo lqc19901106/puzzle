2fdb3959d52399c76339f8304ed81eba962a468c
liuqichao <liuqichao@sinotrans.com>
2020-08-14 18:43:19 +0800

    fix: ui

---
+++ conf/db.js
@@ -0,0 +1,9 @@
+module.export = {
+  mysql: {
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    port: "3306",
+    database: "puzzle",
+  },
+};
--- config/sql.js
+++
@@ -1,7 +0,0 @@
-module.export = {
-    host     : 'localhost',       
-    user     : 'root',              
-    password : '123456',       
-    port: '3306',                   
-    database: 'puzzle' 
-}
